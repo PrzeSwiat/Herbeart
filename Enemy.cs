634da6a70acef244512bef4c2844d01fc4eaf0bf
Michal Banasiak <bananiwo@gmail.com>
2023-04-24 22:18:02 +0200

    change enemy scale for visibility

--- Enemy.cs
+++ Enemy.cs
@@ -20,6 +20,7 @@ namespace TheGame
             Direction = new Vector2(0, 0);
             lastAttackTime = DateTime.Now;
             actualTime = lastAttackTime;
+            SetScale(0.5f);
             
         }
 
