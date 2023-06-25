60c6d80db58c6cb88544196bfdcbf7d430b9324e
Michal Banasiak <bananiwo@gmail.com>
2023-06-26 00:45:40 +0200

    add windup, refactoring, tidy code

--- Enemies/nettle.cs
+++ Enemies/nettle.cs
@@ -9,20 +9,25 @@ namespace TheGame
 {
     internal class Nettle : Enemy
     {
+        private float movementCooldown = 2f;
+        private float windUpCooldown = 0.5f;
+        float armortime = 10; // not used
+
         private DateTime lastAttackTime, actualTime;
         public event EventHandler OnAttack;
-        private int attackCounter = 0;
-        private float attackCooldown = 2f;
-        private float attackCooldownTimer = 0f;
+        private int attackCounter = 0; // not used
+        private float movementTimer = 0f;
+        private float windUpTimer = 0f;
         private bool isMoving = false;
-        float armortime = 10;
+        private bool isWindingUp = false;
         Vector3 playerPosition;
         Vector3 lastPlayerPosition;
         Vector3 directionTowardsTarget;
-        int lastHealth;
+        int lastHealth; // not used
+        private float deltaTime;
         public Nettle(Vector3 worldPosition, string modelFileName, string textureFileName) : base(worldPosition, modelFileName, textureFileName)
         {
-            AssignParameters(6, 12, 12, 1f);
+            AssignParameters(6, 12, 25, 1f);
             this.setBSRadius(2.5f);
             this.shadow.SetScale(0.95f);
             this.visionRange = 25f;
@@ -33,6 +38,7 @@ namespace TheGame
 
         public override void Update(float deltaTime, Player player)
         {
+            this.deltaTime = deltaTime;
             base.Update();
             playerPosition = player.GetPosition();
             HandleStunnedStatus(deltaTime);
@@ -42,24 +48,52 @@ namespace TheGame
             }
             HandleSlowedStatus(deltaTime);
 
-            attackCooldownTimer += deltaTime;
-            if (CheckDistanceAndAttackCooldown(player) && !isMoving)
+            movementTimer += deltaTime;
+            if (CheckDistanceAndMovementCooldown(player) && !isMoving && !isWindingUp)
             {
                 lastPlayerPosition = playerPosition;
-                isMoving = true;
+                isWindingUp = true;
                 directionTowardsTarget = lastPlayerPosition - this.GetPosition();
                 directionTowardsTarget.Normalize();
                 this.Direction = new Vector2(directionTowardsTarget.X, directionTowardsTarget.Z);
                 RotateTowardsCurrentDirection();
             }
+            if (isWindingUp)
+            {
+                WindUp();
+            }
             if (isMoving)
+            {
+                Movement(player);
+            }
+            this.shadow.UpdatingEnemy(this.GetPosition(), new Vector3(1.8f, 0, -1.9f));
+        }
+
+        private void WindUp()
+        {
+            windUpTimer += deltaTime;
+            if (windUpTimer < windUpCooldown)
+                return;
+         
+            windUpTimer = 0f;
+            isWindingUp = false;
+            isMoving = true;
+        }
+
+        private void Movement(Player player)
+        {
+            CheckCollision(player);
+            if (Collides)
+            {
+                StopMovement();
+                Debug.WriteLine("COLLISION");
+                OnAttack?.Invoke(this, EventArgs.Empty);
+            }
+            else
             {
                 CheckArrivedAtTargetPosition(lastPlayerPosition);
                 MoveForwards(deltaTime, true);
             }
-            
-
-            this.shadow.UpdatingEnemy(this.GetPosition(), new Vector3(1.8f, 0, -1.9f));
         }
 
         private bool CheckArrivedAtTargetPosition(Vector3 targetPosition)
@@ -69,15 +103,20 @@ namespace TheGame
             if (!isCloseEnough)
                 return false;
 
-            isMoving = false;
-            attackCooldownTimer = 0f;
+            StopMovement();
             return true;
         }
 
-        private bool CheckDistanceAndAttackCooldown(Player player)
+        private void StopMovement()
+        {
+            isMoving = false;
+            movementTimer = 0f;
+        }
+
+        private bool CheckDistanceAndMovementCooldown(Player player)
         {
             float distanceToTarget = Vector3.Distance(player.GetPosition(), this.GetPosition());
-            if (distanceToTarget < visionRange && attackCooldownTimer > attackCooldown)
+            if (distanceToTarget < visionRange && movementTimer > movementCooldown)
                 return true;
             else
                 return false;
